8b84302788fec189dfb0fec9a20e09cfe0dccff5
marrod22daw <marrod22daw@proton.me>
2022-12-19 00:45:58 +0100

    Commit 6

--- myscript.js
+++ myscript.js
@@ -1,9 +1,7 @@
-
-
-
 let MIDA = 70; // tamaño de cada celda en píxeles
-let MARGE = 30;
+let TOPMARGE = 30;
 let AMPLADA = 492;
+let DiasSemana = ["Dilluns", "Dimarts", "Dimecres", "Dijous", "Divendres", "Dissabte", "Diumenge"];
 
 function dibuixar() {
     let canvas = document.getElementById('calendari');
@@ -28,18 +26,25 @@ function dibuixar() {
         let ctx = canvas.getContext('2d');
         for (let i = 0; i < 31; i++) {
             let x = (i % 7) * MIDA; // x-coordenada de la celda
-            let y = Math.floor(i / 7) * MIDA; // y-coordenada de la celda
+            let y = Math.floor(i / 7) * MIDA + TOPMARGE; // y-coordenada de la celda
             ctx.strokeRect(x, y, MIDA, MIDA);
         }
 
         // Escribe el número de cada día en cada celda
         for (let i = 0; i < 31; i++) {
             let x = (i % 7) * MIDA + MIDA / 2; // x-coordenada del centro de la celda
-            let y = Math.floor(i / 7) * MIDA + MIDA / 2; // y-coordenada del centro de la celda
-            ctx.fillText(i + 1, x, y);
+            let y = Math.floor(i / 7) * MIDA + MIDA + TOPMARGE / 2; // y-coordenada del centro de la celda
+            ctx.fillText(i + 1, x, y - 40);
             console.log(x, y);
         }
+        let j = 0;
+        for (let i = 0; i < 7; i++) {
+            ctx.fillText(DiasSemana[i], j, 20);
+            j += 70
+        }
     }
+
+    geolocalitzacio = document.getElementById("demo");
 }
 
 function dibuixarFigura(ctx, figura, fila, columna) {
@@ -49,7 +54,7 @@ function dibuixarFigura(ctx, figura, fila, columna) {
     /* let x = (i % 7) * MIDA + MIDA / 2;
     let y = Math.floor(i / 7) * MIDA + MIDA / 2; */
     img.onload = function () {
-        ctx.drawImage(img, fila * MIDA, columna * MIDA, MIDA, MIDA);
+        ctx.drawImage(img, fila * MIDA, columna * MIDA + TOPMARGE, MIDA, MIDA);
     }
 }
 
@@ -105,6 +110,18 @@ function gestionarFinalDrag2(ev) {
 
 
 }
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+}
+
+function showPosition(position) {
+    geolocalitzacio.innerHTML = "Latitud: " + position.coords.latitude +
+        "<br>Longitut: " + position.coords.longitude;
+}
+
 
 
 window.addEventListener('load', dibuixar, false);
